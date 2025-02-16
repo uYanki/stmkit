975386cf094d9ce3694ba488298140b0023a124f
uyk <88850669+uYanki@users.noreply.github.com>
2025-02-16 17:58:45 +0800

    1

---
+++ embsw/examples/stm32f407vet6/FreeMaster/uart/Core/Inc/projconf.h
@@ -0,0 +1,34 @@
+#ifndef __PROJ_CONF_H__
+#define __PROJ_CONF_H__
+
+#include "stm32f4xx_hal.h"
+#include "sdkopts.h"
+#include "board.h"
+
+/**
+ * @brief SDK Conf
+ * @{
+ */
+
+#define CONFIG_ARCH_TYPE ARCH_ARM_CM4
+
+/**
+ * @brief logger
+ */
+
+#define PRINTF(...)
+
+/**
+ * @}
+ */
+
+/**
+ * @brief Project Conf
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+#endif  // !__PROJ_CONF_H__
---
+++ embsw/examples/stm32f407vet6/linkscope/Core/Inc/projconf.h
@@ -0,0 +1,33 @@
+#ifndef __PROJ_CONF_H__
+#define __PROJ_CONF_H__
+
+#include "stm32f4xx_hal.h"
+#include "sdkopts.h"
+
+/**
+ * @brief SDK Conf
+ * @{
+ */
+
+#define CONFIG_ARCH_TYPE ARCH_ARM_CM4
+
+/**
+ * @brief logger
+ */
+
+#define PRINTF(...)
+
+/**
+ * @}
+ */
+
+/**
+ * @brief Project Conf
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+#endif  // !__PROJ_CONF_H__
