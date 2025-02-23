20b8886ba2692d686576588a56ad266effb9bf05
uyk <88850669+uYanki@users.noreply.github.com>
2025-02-23 23:55:06 +0800

    1

---
+++ examples/stm32f103c8t6/stmusb/usbd_msc_onchip_flash/Core/Inc/gconf.h
@@ -0,0 +1,21 @@
+#ifndef __GSDK_CONF_H__
+#define __GSDK_CONF_H__
+
+#include "gopts.h"
+
+#define CONFIG_ARM_DWT_SW       0
+#define CONFIG_ARM_TSG_SW       0
+
+#define CONFIG_DEMOS_SW         1
+
+#define CONFIG_SWUART_MODULE_SW 1
+#define CONFIG_SWI2C_MODULE_SW  1
+#define CONFIG_SWSPI_MODULE_SW  1
+
+#define CONFIG_HWUART_MODULE_SW 1
+#define CONFIG_HWI2C_MODULE_SW  0
+#define CONFIG_HWSPI_MODULE_SW  1
+
+#define CONFIG_HWCAN_MODULE_SW  1
+
+#endif /*<! __GSDK_CONF_H__ */
